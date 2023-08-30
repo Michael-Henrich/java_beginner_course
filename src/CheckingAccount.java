85dcdae2c789540c49d4f05e0b4d01576fb2ac93
Michael Henrich <michael.henrich@dedalus.com>
2023-08-30 11:11:01 +0200

    Added Banking classes

---
+++ src/CheckingAccount.java
@@ -0,0 +1,24 @@
+public class CheckingAccount extends Account{
+    public int rewardPoints;
+    public CheckingAccount(double balance, double interestRate, int rewardPoints) {
+        super(balance,interestRate);
+        this.rewardPoints = rewardPoints;
+    }
+
+    public boolean purchase(double cost){
+        if(cost > balance){
+            return false;
+        }
+        balance -= cost;
+        rewardPoints += calculateRewardPoints(cost);
+        return true;
+    }
+
+    public int calculateRewardPoints(double cost){
+        return (int) (cost*10);
+    }
+
+    public int getRewardPoints() {
+        return rewardPoints;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
+        SilverCheckingAccount silver  = new SilverCheckingAccount(5000,0.025, 1000);
+        GoldCheckingAccount gold  = new GoldCheckingAccount(5000,0.025, 1000);
+        DiamondCheckingAccount diamond  = new DiamondCheckingAccount(5000,0.025, 1000);
 
+        System.out.println(silver.getRewardPoints());
+        System.out.println(gold.getRewardPoints());
+        System.out.println(diamond.getRewardPoints());
+
+        silver.purchase(50);
+        gold.purchase(50);
+        diamond.purchase(50);
+
+        System.out.println(silver.getRewardPoints());
+        System.out.println(gold.getRewardPoints());
+        System.out.println(diamond.getRewardPoints());
     }
 
 }
